9ac1cffed842669514eb7a019bfc8869024914ff
slopez <www.sai@gmail.com>
2016-09-07 06:21:25 +0000

    Added the right number of lines for the useless_questions.txt

--- bullyme.sh
+++ bullyme.sh
@@ -3,7 +3,8 @@
 
 #read from a file a specific file
 #take a random number between 1 and the file's number of lines
-random_line=$((1 + RANDOM % 10));
+number_of_lines=`cat useless_questions.txt | wc -l`;
+random_line=$((1 + RANDOM % $number_of_lines));
 #show the file line
 sed $random_line'q;d' useless_questions.txt;
 #add further validations for file exists, real line randomic and so
