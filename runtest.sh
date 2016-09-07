e8ae500fbf7071210b2842a54932c223c62241d7
Saidel Lopez <sailopher@yahoo.com.mx>
2016-09-07 00:43:55 -0500

    Create runtest.sh
    Kickstart to this file

---
+++ runtest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# declare run test script
+
+#read from the setup file the path to
+#check if the $BULLYNG_PATH is set
+#questions files
+#answers files
+#bullyme.sh
+#answerme.sh
+
+#check if the alias are set rightly
+
+#if everything was setup rightly the send a message saying so
+#otherwise send a message with all the setup checks that failed
