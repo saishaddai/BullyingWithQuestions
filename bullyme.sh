b6c8c0ee6da419047467d2021713966f8ac547fd
Saidel Lopez <sailopher@yahoo.com.mx>
2016-09-07 00:56:27 -0500

    Update bullyme.sh

--- bullyme.sh
+++ bullyme.sh
@@ -3,6 +3,8 @@
 
 #read from a file a specific line
 #a random line by the way
+random_line = $((1 + RANDOM % 10))
 #and simply show it
+sed '$random_lineq;d' file
 
 #add further validations for file exists, real line randomic and so
