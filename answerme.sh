6ad254a1b06f2531bc182cc9e459bef42cb307d0
Saidel Lopez <sailopher@yahoo.com.mx>
2016-09-07 00:38:03 -0500

    Create answerme.sh
    kickstart for answer.sh

---
+++ answerme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# declare answerme script 
+
+#read from a file a specific line
+#read the same line a question was made and show the answer
+#if there is no answer for this line show a 'answer not found'
+
+#in complex versions this will read from an specific file and then the right line
+#it will include a file name and file line as a hash 
+# for now on it will only read from the same file, useless_answers.txt
