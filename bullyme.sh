49dc6a65d30544461e73ed8c6a45bfa59dd61e2c
Saidel Lopez <sailopher@yahoo.com.mx>
2016-09-07 00:29:37 -0500

    Create bullyme.sh
    kick start with this file

---
+++ bullyme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# declare bullyme script 
+
+#read from a file a specific line
+#a random line by the way
+#and simply show it
+
+#add further validations for file exists, real line randomic and so
