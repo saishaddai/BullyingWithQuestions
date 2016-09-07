960358100ba94ba89a8bb931249b485a2b1beb17
Saidel Lopez <sailopher@yahoo.com.mx>
2016-09-07 00:46:00 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# declare setup script 
+
+#this file will include the name of the questions files
+# and answers files
+
+#for the moment it will only include the useless_questions.txt and useless_answers.txt
