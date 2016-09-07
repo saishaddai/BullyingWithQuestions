7a1e557896a34ac37016514dd3f6e2492d5c7637
slopez <www.sai@gmail.com>
2016-09-07 06:11:08 +0000

    Added a real code to retrieve a question from the useless_questions file /bin/bash: q: command not found

--- bullyme.sh
+++ bullyme.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # declare bullyme script 
 
-#read from a file a specific line
-#a random line by the way
-random_line = $((1 + RANDOM % 10))
-#and simply show it
-sed '$random_lineq;d' file
-
+#read from a file a specific file
+#take a random number between 1 and the file's number of lines
+random_line=$((1 + RANDOM % 10));
+#show the file line
+sed $random_line'q;d' useless_questions.txt;
 #add further validations for file exists, real line randomic and so
